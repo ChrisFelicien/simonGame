1d0c7f7a99474492cc5d2ee9e5889e2a6a4c016e
Chris Felicien <f.ilungakalenga@gmail.com>
2022-06-26 22:24:07 +0200

    Adding sound on button click

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
+
+// Functions
 const nextSequence = function () {
   const randomNumber = Math.floor(Math.random() * 4); //Generate a number
   const randomChosenColour = buttonColours[randomNumber]; // set color
-
-  $(`div#${randomChosenColour}`).css("background", "black");
-
+  const soundId = $(this).attr("id");
+  playSong(soundId);
   gamePattern.push(randomChosenColour);
   return gamePattern;
 };
 
-const randomNumber = nextSequence;
-console.log(randomNumber());
+const playSong = function (el) {
+  const audio = new Audio(`./sounds/${el}.mp3`);
+  return audio.play();
+};
+
+$(".btn").on("click", nextSequence);
