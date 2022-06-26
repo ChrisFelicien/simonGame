b058f44e24d2118968794768e651239077b14a60
Chris Felicien <f.ilungakalenga@gmail.com>
2022-06-26 22:58:19 +0200

    Adding effect on btn click

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ const userClickedPattern = [];
 
 // Functions
 const nextSequence = function () {
+  addClass(this, "pressed");
+  removeClass(this, "pressed");
   const randomNumber = Math.floor(Math.random() * 4); //Generate a number
   const randomChosenColour = buttonColours[randomNumber]; // set color
   const userChosenColour = $(this).attr("id");
   playSong(userChosenColour);
   gamePattern.push(randomChosenColour);
   userClickedPattern.push(userChosenColour);
-  console.log(gamePattern);
-  console.log(userClickedPattern);
   return gamePattern;
 };
 
-const playSong = function (el) {
-  const audio = new Audio(`./sounds/${el}.mp3`);
+const playSong = function (name) {
+  const audio = new Audio(`./sounds/${name}.mp3`);
   return audio.play();
 };
 
+const addClass = function (el, className) {
+  return $(el).addClass(className);
+};
+const removeClass = function (el, className) {
+  return setTimeout(() => {
+    return $(el).removeClass(className);
+  }, 100);
+};
 $(".btn").on("click", nextSequence);
