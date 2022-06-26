65fe8743d40bf21ebb614b3382bbdeb073104e45
Chris Felicien <f.ilungakalenga@gmail.com>
2022-06-26 23:27:20 +0200

    Updata level

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
+let level = 0;
 
 // Functions
 const nextSequence = function () {
+  $("#level-title").text(`Level ${level}`);
   addClass(this, "pressed");
   removeClass(this, "pressed");
   const randomNumber = Math.floor(Math.random() * 4); //Generate a number
@@ -12,7 +14,8 @@ const nextSequence = function () {
   playSong(userChosenColour);
   gamePattern.push(randomChosenColour);
   userClickedPattern.push(userChosenColour);
-  return gamePattern;
+  level++;
+  return upDateScore($("#level-title"), level);
 };
 
 const playSong = function (name) {
@@ -28,11 +31,16 @@ const removeClass = function (el, className) {
     return $(el).removeClass(className);
   }, 100);
 };
-const startGame = function () {
-  $(".btn").on("click", nextSequence);
-  $("#level-title").text("Click on the button");
+const upDateLevel = function (tag, score) {
+  return $(tag).html(`Level ${score}`);
 };
-
+const upDateScore = function (tag, score) {
+  return $(tag).html(`Level ${score}`);
+};
+const startGame = function () {
+  upDateLevel($("#level-title"), level);
+  $(".btn").on("click", nextSequence);
+};
 $(document).on("keypress", (e) => {
   e.key.toLowerCase() != "a" ? "" : startGame();
 });
